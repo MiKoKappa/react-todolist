328446ca0aa6b3c2cb9dd7f36be555611ff1b405
MiKoKappa <mikopar@gmail.com>
2020-07-16 13:25:50 +0200

    Updates

---
+++ precache-manifest.e6447e2045b3aace4a17cb62ae82df84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75054614d557a938f07f443c998072ee",
+    "url": "/react-todolist/index.html"
+  },
+  {
+    "revision": "21f7b470ddefb7ca570e",
+    "url": "/react-todolist/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c9a41cd2e1e1160a70c1",
+    "url": "/react-todolist/static/js/2.66b1d721.chunk.js"
+  },
+  {
+    "revision": "58aabe32aae325c8d2caf2ff6956fc48",
+    "url": "/react-todolist/static/js/2.66b1d721.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21f7b470ddefb7ca570e",
+    "url": "/react-todolist/static/js/main.466cebd3.chunk.js"
+  },
+  {
+    "revision": "c8e9edcc0b7d98c328a3",
+    "url": "/react-todolist/static/js/runtime-main.5ff039be.js"
+  }
+]);
